d4ebe5f336ac77d0282f4f7ddb47ca1321ac0354
Nethz Jimenez <bynethz@gmail.com>
2024-06-30 16:51:18 -0500

    chore: Update package dependencies and environment configuration

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,14 +4,20 @@ import * as Joi from 'joi';
 interface EnvVars {
   PORT: number;
   DATABASE_URL: string;
+  NATS_SERVERS: string[];
 }
 
 const envVarsSchema = Joi.object({
   PORT: Joi.number().default(3000),
   DATABASE_URL: Joi.string().required(),
+
+  NATS_SERVERS: Joi.array().items(Joi.string()).required(),
 }).unknown(true);
 
-const { error, value } = envVarsSchema.validate(process.env);
+const { error, value } = envVarsSchema.validate({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
+});
 
 if (error) {
   throw new Error(`Config validation error: ${error.message}`);
@@ -22,4 +28,5 @@ const envVars: EnvVars = value;
 export const envs = {
   PORT: envVars.PORT,
   DATABASE_URL: envVars.DATABASE_URL,
+  NATS_SERVERS: envVars.NATS_SERVERS,
 };
